75033160e0743fa85a1d2513894af34106adfe51
marcknova <marck.nova@gmail.com>
2023-03-14 22:59:51 -0600

    react router

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <div className="bg-[#ff97d9]">
         <nav className="flex justify-between mx-2">
             <div className="p-1 m-2">
+              <Link to="/">
                 <h1 className="text-2xl font-bold">Logo</h1>
+              </Link>
             </div>
             <ul className="list-none flex space-x-7 p-2 mt-2">
-                    <li>Lo mas nuevo</li>
-                    <li>Mas vendido</li>
-                    <li>Blog</li>
-                    <li>Cara</li>
-                    <li>Labios</li>
-                    <li>Ojos</li>
+                    <Link to="/masnuevo">Lo mas nuevo</Link>
+                    <Link to="/masvendido">Mas vendido</Link>
+                    <Link to="/blog">Blog</Link>
+                    <Link to="/cara">Cara</Link>
+                    <Link to="/labios">Labios</Link>
+                    <Link to="/ojos">Ojos</Link>
             </ul>
             <div className="p-2 m-2">
                 <span className="mx-5"><i className="fa-solid fa-user"></i></span>
